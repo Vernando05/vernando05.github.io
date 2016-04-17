75740abdb5076312bfaac66aea845891f51f1581
vernando05 <usedgrey05@gmail.com>
2016-04-17 09:46:20 +0700

    run particles when page fully loaded

--- src/_site/js/main.js
+++ src/_site/js/main.js
@@ -8,6 +8,9 @@
 	function loadPageStatus() {
 		window.onload = function () {
 			window.loaded = true;
+			if(document.getElementById("particles-object")){
+				particleSquare();
+			}		
 			if (timeout) {
 				onCompleteLoad();
 			}
@@ -34,7 +37,114 @@
 			animationEnd = true;
 		});
 	}
-	$(document).ready( function () {
+	function particleSquare() {
+		var container;
+		var camera, scene, renderer, particles, geometry, materials = [], parameters, i, h, color, size;
+		var mouseX = 0, mouseY = 0;
+		var windowHalfX = window.innerWidth / 2;
+		var windowHalfY = window.innerHeight / 2;
+		var objImg = document.getElementById('particles-object');
+		var objImgHeight = objImg.clientHeight;
+		var objImgWidth = objImg.clientWidth;
+
+		init();
+
+		animate();
+
+		function init() {
+			container = document.createElement('div');
+			container.id = 'bg-particles';
+			document.getElementById('particles-wrap').appendChild(container);
+			camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 3000);
+			camera.position.z = 1000;
+			scene = new THREE.Scene();
+			scene.fog = new THREE.FogExp2(0xffffff, 0);
+			geometry = new THREE.Geometry();
+			for (i = 0; i < 1000; i ++) {
+				var vertex = new THREE.Vector3();
+				vertex.x = Math.random() * 2000 - 1000;
+				vertex.y = Math.random() * 2000 - 1000;
+				vertex.z = Math.random() * 2000 - 1000;
+				geometry.vertices.push(vertex);
+			}
+			parameters = [
+				[0.30, 5],
+				[0.45, 4],
+				[0.60, 3],
+				[0.75, 2],
+				[0.80, 1]
+			];
+			for (i = 0; i < parameters.length; i ++) {
+				color = parameters[i][0];
+				size  = parameters[i][1];
+				materials[i] = new THREE.PointsMaterial({ size: size });
+				particles = new THREE.Points(geometry, materials[i]);
+				particles.rotation.x = Math.random() * 6;
+				particles.rotation.y = Math.random() * 6;
+				particles.rotation.z = Math.random() * 6;
+				scene.add(particles);
+			}
+			renderer = new THREE.WebGLRenderer({alpha: true});
+			renderer.setClearColor(0xffffff, 0);
+			renderer.setPixelRatio(window.devicePixelRatio);
+			renderer.setSize(objImgWidth, objImgHeight);
+			container.appendChild(renderer.domElement);
+			document.addEventListener('mousemove', onDocumentMouseMove, false);
+			document.addEventListener('touchstart', onDocumentTouchStart, false);
+			document.addEventListener('touchmove', onDocumentTouchMove, false);		
+			window.addEventListener('resize', onWindowResize, false);
+		}
+		function onWindowResize() {
+			windowHalfX = window.innerWidth / 2;
+			windowHalfY = window.innerHeight / 2;
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+			objImgWidth = objImg.clientHeight;
+			objImgHeight = objImg.clientWidth;
+			renderer.setSize(objImgWidth, objImgHeight);
+		}
+		function onDocumentMouseMove(event) {
+			mouseX = event.clientX - windowHalfX;
+			mouseY = event.clientY - windowHalfY;
+		}
+		function onDocumentTouchStart(event) {
+			if ( event.touches.length === 1 ) {
+				event.preventDefault();
+				mouseX = event.touches[ 0 ].pageX - windowHalfX;
+				mouseY = event.touches[ 0 ].pageY - windowHalfY;
+			}
+		}
+		function onDocumentTouchMove(event) {
+			if (event.touches.length === 1) {
+				event.preventDefault();
+				mouseX = event.touches[0].pageX - windowHalfX;
+				mouseY = event.touches[0].pageY - windowHalfY;
+			}
+		}
+		function animate() {
+			requestAnimationFrame(animate);
+			render();
+		}
+		function render() {
+			var time = Date.now() * 0.00005;
+			camera.position.x += (mouseX - camera.position.x) * 0.05;
+			camera.position.y += (-mouseY - camera.position.y) * 0.05;
+			camera.lookAt(scene.position);
+			for (i = 0; i < scene.children.length; i ++) {
+				var object = scene.children[i];
+				if (object instanceof THREE.Points) {
+					object.rotation.y = time * (i < 4 ? i + 1 : - ( i + 1 ));
+				}
+			}
+			for (i = 0; i < materials.length; i ++) {
+				color = parameters[i][0];
+				//h = ( 360 * ( color[0] + time ) % 360 ) / 360;
+				materials[i].color.setHSL(0, 0, color);
+			}
+			renderer.render(scene, camera);
+		}
+	}
+	$(document).ready(function () {
 		
 		transformicons.add('.tcon');
 		$('.tcon-menu--xcross').click(function () {
@@ -78,6 +188,9 @@
 					$main.load(href + ' .main-page', function () {
 						onCompleteLoad();
 						contactForm();
+						if(document.getElementById("particles-object")){
+							particleSquare();
+						}
 					});
 				});
 			}
@@ -195,106 +308,4 @@
 			$(".bg-page").css('transform', 'translate(' + x + 'px,' + y + 'px)');
 		});
 	});
-	var container;
-	var camera, scene, renderer, particles, geometry, materials = [], parameters, i, h, color, size;
-	var mouseX = 0, mouseY = 0;
-	var windowHalfX = window.innerWidth / 2;
-	var windowHalfY = window.innerHeight / 2;
-	var objectHeight = document.getElementById('particles-object').clientHeight;
-	var objectWidth = document.getElementById('particles-object').clientWidth;
-	init();
-	animate();
-	function init() {
-		container = document.createElement( 'div' );
-		container.id = 'bg-particles';
-		document.getElementById('particles-wrap').appendChild( container );
-		camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 3000 );
-		camera.position.z = 1000;
-		scene = new THREE.Scene();
-		scene.fog = new THREE.FogExp2( 0xffffff, 0 );
-		geometry = new THREE.Geometry();
-		for ( i = 0; i < 1000; i ++ ) {
-			var vertex = new THREE.Vector3();
-			vertex.x = Math.random() * 2000 - 1000;
-			vertex.y = Math.random() * 2000 - 1000;
-			vertex.z = Math.random() * 2000 - 1000;
-			geometry.vertices.push( vertex );
-		}
-		parameters = [
-			[ 0.30, 5 ],
-			[ 0.45, 4 ],
-			[ 0.60, 3 ],
-			[ 0.75, 2 ],
-			[ 0.80, 1 ]
-		];
-		for ( i = 0; i < parameters.length; i ++ ) {
-			color = parameters[i][0];
-			size  = parameters[i][1];
-			materials[i] = new THREE.PointsMaterial( { size: size } );
-			particles = new THREE.Points( geometry, materials[i] );
-			particles.rotation.x = Math.random() * 6;
-			particles.rotation.y = Math.random() * 6;
-			particles.rotation.z = Math.random() * 6;
-			scene.add( particles );
-		}
-		renderer = new THREE.WebGLRenderer({alpha: true});
-		renderer.setClearColor( 0xffffff, 0);
-		renderer.setPixelRatio( window.devicePixelRatio );
-		renderer.setSize( objectWidth, objectHeight );
-		container.appendChild( renderer.domElement );
-		document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-		document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-		document.addEventListener( 'touchmove', onDocumentTouchMove, false );		
-		window.addEventListener( 'resize', onWindowResize, false );
-	}
-	function onWindowResize() {
-		windowHalfX = window.innerWidth / 2;
-		windowHalfY = window.innerHeight / 2;
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		objectHeight = document.getElementById('particles-object').clientHeight;
-		objectWidth = document.getElementById('particles-object').clientWidth;
-		renderer.setSize( objectWidth, objectHeight );
-	}
-	function onDocumentMouseMove( event ) {
-		mouseX = event.clientX - windowHalfX;
-		mouseY = event.clientY - windowHalfY;
-	}
-	function onDocumentTouchStart( event ) {
-		if ( event.touches.length === 1 ) {
-			event.preventDefault();
-			mouseX = event.touches[ 0 ].pageX - windowHalfX;
-			mouseY = event.touches[ 0 ].pageY - windowHalfY;
-		}
-	}
-	function onDocumentTouchMove( event ) {
-		if ( event.touches.length === 1 ) {
-			event.preventDefault();
-			mouseX = event.touches[ 0 ].pageX - windowHalfX;
-			mouseY = event.touches[ 0 ].pageY - windowHalfY;
-		}
-	}
-	
-	function animate() {
-		requestAnimationFrame( animate );
-		render();
-	}
-	function render() {
-		var time = Date.now() * 0.00005;
-		camera.position.x += ( mouseX - camera.position.x ) * 0.05;
-		camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
-		camera.lookAt( scene.position );
-		for ( i = 0; i < scene.children.length; i ++ ) {
-			var object = scene.children[ i ];
-			if ( object instanceof THREE.Points ) {
-				object.rotation.y = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
-			}
-		}
-		for ( i = 0; i < materials.length; i ++ ) {
-			color = parameters[i][0];
-			//h = ( 360 * ( color[0] + time ) % 360 ) / 360;
-			materials[i].color.setHSL( 0, 0, color);
-		}
-		renderer.render( scene, camera );
-	}
 })();
